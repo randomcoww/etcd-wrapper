50bcf86f9dbd9aed94028a7d8e7dac3533fa8c97
randomcoww <averyfuzzybunny@gmail.com>
2024-02-23 23:49:20 +0000

    WIP updating test and mock

--- pkg/etcdutil/mock.go
+++ pkg/etcdutil/mock.go
@@ -6,32 +6,39 @@ import (
 	"io"
 )
 
+type StatusResponseWithErr struct {
+	*etcdserverpb.StatusResponse
+	Err error
+}
+
+type MemberListResponseWithErr struct {
+	Header *etcdserverpb.ResponseHeader
+	Err    error
+}
+
+type MemberAddResponseWithErr struct {
+	Header *etcdserverpb.ResponseHeader
+	Member *etcdserverpb.Member
+	Err    error
+}
+
+type MockNode struct {
+	MemberResponse        Member
+	StatusResponseWithErr *StatusResponseWithErr
+}
+
 type MockClient struct {
-	EndpointsResponse []string
-	SyncEndpointsErr  error
-	StatusResponses   []struct {
-		*etcdserverpb.StatusResponse
-		Err error
-	}
-	MemberListResponse struct {
-		*etcdserverpb.MemberListResponse
-		Err error
-	}
-	MemberAddResponse struct {
-		*etcdserverpb.MemberAddResponse
-		Err error
-	}
-	MemberRemoveResponse struct {
-		*etcdserverpb.MemberRemoveResponse
-		Err error
-	}
-	MemberPromoteResponse struct {
-		*etcdserverpb.MemberPromoteResponse
-		Err error
-	}
-	HealthCheckErr    error
-	DefragmentErr     error
-	CreateSnapshotErr error
+	NodeEndpointMap              map[string]*MockNode
+	EndpointsResponse            []string
+	SyncEndpointsErr             error
+	MemberAddResponseWithErr     *MemberAddResponseWithErr
+	MemberListResponseWithErr    *MemberListResponseWithErr
+	MemberRemoveResponseWithErr  *MemberListResponseWithErr
+	MemberPromotResponseWitheErr *MemberListResponseWithErr
+	HealthCheckErr               error
+	DefragmentErr                error
+	CreateSnapshotErr            error
+	endpoints                    []string
 }
 
 func (m *MockClient) Close() error {
@@ -39,6 +46,7 @@ func (m *MockClient) Close() error {
 }
 
 func (m *MockClient) Sync(ctx context.Context) error {
+	m.EndpointsResponse = m.endpoints
 	return nil
 }
 
@@ -47,29 +55,73 @@ func (m *MockClient) Endpoints() []string {
 }
 
 func (m *MockClient) SyncEndpoints() error {
-	return m.SyncEndpointsErr
+	if m.SyncEndpointsErr != nil {
+		return m.SyncEndpointsErr
+	}
+	m.Sync(context.Background())
+	return nil
 }
 
 func (m *MockClient) Status(handler func(Status, error)) {
-	for _, resp := range m.StatusResponses {
-		handler(resp.StatusResponse, resp.Err)
+	nodeResps := m.getUniqueNodes(m.Endpoints())
+	for _, resp := range nodeResps {
+		handler(resp.StatusResponseWithErr.StatusResponse, resp.StatusResponseWithErr.Err)
 	}
 }
 
 func (m *MockClient) ListMembers() (List, error) {
-	return m.MemberListResponse.MemberListResponse, m.MemberListResponse.Err
+	var membersResp []*etcdserverpb.Member
+	nodeResps := m.getUniqueNodes(m.Endpoints())
+	for _, resp := range nodeResps {
+		membersResp = append(membersResp, resp.Member)
+		m.endpoints = append(m.endpoints, resp.GetClientURLs()...)
+	}
+	return &etcdserverpb.MemberListResponse{
+		Header:  MemberListResponseWithErr.Header,
+		Members: membersResp,
+	}, m.MemberListResponseWithErr.Err
 }
 
 func (m *MockClient) AddMember(peerURLs []string) (List, Member, error) {
-	return m.MemberAddResponse.MemberAddResponse, m.MemberAddResponse.GetMember(), m.MemberAddResponse.Err
+	var membersResp []*etcdserverpb.Member
+	nodeResps := m.getUniqueNodes(m.Endpoints())
+	for _, resp := range nodeResps {
+		membersResp = append(membersResp, resp.Member)
+		m.endpoints = append(m.endpoints, resp.GetClientURLs()...)
+	}
+	return &etcdserverpb.MemberAddResponse{
+		Header:  MemberAddResponseWithErr.Header,
+		Members: membersResp,
+	}, m.MemberAddResponseWithErr.Member, m.MemberAddResponseWithErr.Err
 }
 
 func (m *MockClient) RemoveMember(id uint64) (List, error) {
-	return m.MemberRemoveResponse.MemberRemoveResponse, m.MemberRemoveResponse.Err
+	var membersResp []Member
+	nodeResps := m.getUniqueNodes(m.Endpoints())
+	for _, resp := range nodeResps {
+		if resp.Member.GetID() == id {
+			continue
+		}
+		membersResp = append(membersResp, resp.Member)
+		m.endpoints = append(m.endpoints, resp.GetClientURLs()...)
+	}
+	return &etcdserverpb.MemberRemoveResponse{
+		Header:  MemberRemoveResponseWithErr.Header,
+		Members: membersResp,
+	}, MemberRemoveResponseWithErr.Err
 }
 
 func (m *MockClient) PromoteMember(id uint64) (List, error) {
-	return m.MemberPromoteResponse.MemberPromoteResponse, m.MemberPromoteResponse.Err
+	var membersResp []*etcdserverpb.Member
+	nodeResps := m.getUniqueNodes(m.Endpoints())
+	for _, resp := range nodeResps {
+		membersResp = append(membersResp, resp.Member)
+		m.endpoints = append(m.endpoints, resp.GetClientURLs()...)
+	}
+	return &etcdserverpb.MemberPromoteResponse{
+		Header:  MemberPromoteResponseWithErr.Header,
+		Members: membersResp,
+	}, MemberPromoteResponseWithErr.Err
 }
 
 func (m *MockClient) HealthCheck() error {
@@ -82,4 +134,17 @@ func (m *MockClient) Defragment(endpoint string) error {
 
 func (m *MockClient) CreateSnapshot(handler func(context.Context, io.Reader) error) error {
 	return m.CreateSnapshotErr
+}
+
+func (m *MockClient) getUniqueNodes(endpoints []string) []*Node {
+	var nodes []*Node
+	uniqueNodes := make(map[*Node]struct{})
+	for _, endpoint := range endpoints {
+		nodeResp := m.NodeEndpointMap[endpoint]
+		if _, ok := uniqueNodes[*Node]; nodeResp != nil && !ok {
+			uniqueNodes[nodeResp] = struct{}{}
+			nodes = append(nodes, nodeResp)
+		}
+	}
+	return nodes
 }
--- pkg/status/status_test.go
+++ pkg/status/status_test.go
@@ -9,13 +9,111 @@ import (
 )
 
 func TestNewStatus(t *testing.T) {
+	happyNode0 := &etcdutil.Node{
+		MemberResponse: &etcdserverpb.Member{
+			ID:   1001,
+			Name: "node0",
+			PeerURLs: []string{
+				"https//10.0.0.1:8001",
+				"https//127.0.0.1:8001",
+			},
+			ClientURLs: []string{
+				"https//10.0.0.1:8081",
+				"https//127.0.0.1:8081",
+			},
+			IsLearner: false,
+		},
+		StatusResponseWithErr: &etcdutil.StatusResponseWithErr{
+			StatusResponse: &etcdserverpb.StatusResponse{
+				Header: &etcdserverpb.ResponseHeader{
+					ClusterId: 3001,
+					MemberId:  1001,
+					Revision:  5000,
+				},
+				Leader:    1001,
+				RaftIndex: 2001,
+				IsLearner: false,
+			},
+			Err: nil,
+		},
+	}
+
+	happyNode1 := &etcdutil.Node{
+		MemberResponse: &etcdserverpb.Member{
+			ID:   1002,
+			Name: "node1",
+			PeerURLs: []string{
+				"https//10.0.0.2:8001",
+				"https//127.0.0.1:8001",
+			},
+			ClientURLs: []string{
+				"https//10.0.0.2:8081",
+				"https//127.0.0.1:8081",
+			},
+			IsLearner: false,
+		},
+		StatusResponseWithErr: &etcdutil.StatusResponseWithErr{
+			StatusResponse: &etcdserverpb.StatusResponse{
+				Header: &etcdserverpb.ResponseHeader{
+					ClusterId: 3001,
+					MemberId:  1002,
+					Revision:  5000,
+				},
+				Leader:    1001,
+				RaftIndex: 2001,
+				IsLearner: false,
+			},
+			Err: nil,
+		},
+	}
+
+	happyNode2 := &etcdutil.Node{
+		MemberResponse: &etcdserverpb.Member{
+			ID:   1003,
+			Name: "node1",
+			PeerURLs: []string{
+				"https//10.0.0.3:8001",
+				"https//127.0.0.1:8001",
+			},
+			ClientURLs: []string{
+				"https//10.0.0.3:8081",
+				"https//127.0.0.1:8081",
+			},
+			IsLearner: false,
+		},
+		StatusResponseWithErr: &etcdutil.StatusResponseWithErr{
+			StatusResponse: &etcdserverpb.StatusResponse{
+				Header: &etcdserverpb.ResponseHeader{
+					ClusterId: 3001,
+					MemberId:  1003,
+					Revision:  5000,
+				},
+				Leader:    1001,
+				RaftIndex: 2001,
+				IsLearner: false,
+			},
+			Err: nil,
+		},
+	}
+
+	newMember3 := &etcdserverpb.Member{
+		ID:   1004,
+		Name: "",
+		PeerURLs: []string{
+			"https//10.0.0.4:8001",
+			"https//127.0.0.1:8001",
+		},
+		ClientURLs: []string{},
+		IsLearner:  false,
+	}
+
 	tests := []struct {
-		label             string
-		args              *arg.Args
-		mockClient        *etcdutil.MockClient
-		expectedSelf      *Member
-		expectedLeader    *Member
-		expectedMemberMap map[uint64]*Member
+		label        string
+		args         *arg.Args
+		mockClient   *etcdutil.MockClient
+		expectedSelf *Member
+		// expectedLeader    *Member
+		// expectedMemberMap map[uint64]*Member
 	}{
 		{
 			label: "happy path",
@@ -41,240 +139,53 @@ func TestNewStatus(t *testing.T) {
 				},
 			},
 			mockClient: &etcdutil.MockClient{
+				NodeEndpointMap: map[string]*etcdutil.MockNode{
+					"https://10.0.0.1:8081": happyNode0,
+					"https://10.0.0.2:8081": happyNode1,
+					"https://10.0.0.3:8081": happyNode2,
+				},
 				EndpointsResponse: []string{
 					"https://10.0.0.1:8081",
-					"https://127.0.0.1:8081",
+					"https://10.0.0.2:8081",
+					"https://10.0.0.3:8081",
 				},
 				SyncEndpointsErr: nil,
-				StatusResponses: []struct {
-					*etcdserverpb.StatusResponse
-					Err error
-				}{
-					struct {
-						*etcdserverpb.StatusResponse
-						Err error
-					}{
-						StatusResponse: &etcdserverpb.StatusResponse{
-							Header: &etcdserverpb.ResponseHeader{
-								ClusterId: 3001,
-								MemberId:  1001,
-								Revision:  5000,
-							},
-							Leader:    1001,
-							RaftIndex: 2001,
-							IsLearner: false,
-						},
-						Err: nil,
+				MemberListErr:    nil,
+				MemberAddResponseWithErr: &etcdutil.MemberAddResponseWithErr{
+					Header: &etcdserverpb.ResponseHeader{
+						ClusterId: 10,
+						MemberId:  1,
+						Revision:  20,
+						RaftTerm:  30,
 					},
-					struct {
-						*etcdserverpb.StatusResponse
-						Err error
-					}{
-						StatusResponse: &etcdserverpb.StatusResponse{
-							Header: &etcdserverpb.ResponseHeader{
-								ClusterId: 3001,
-								MemberId:  1002,
-								Revision:  5001,
-							},
-							Leader:    1001,
-							RaftIndex: 2001,
-							IsLearner: false,
-						},
-						Err: nil,
-					},
-					struct {
-						*etcdserverpb.StatusResponse
-						Err error
-					}{
-						StatusResponse: &etcdserverpb.StatusResponse{
-							Header: &etcdserverpb.ResponseHeader{
-								ClusterId: 3001,
-								MemberId:  1003,
-								Revision:  5000,
-							},
-							Leader:    1001,
-							RaftIndex: 2001,
-							IsLearner: false,
-						},
-						Err: nil,
-					},
+					Member: newMember3,
+					Err:    nil,
 				},
-				MemberListResponse: struct {
-					*etcdserverpb.MemberListResponse
-					Err error
-				}{
-					MemberListResponse: &etcdserverpb.MemberListResponse{
-						Header: &etcdserverpb.ResponseHeader{
-							ClusterId: 3001,
-							MemberId:  1001,
-							Revision:  5000,
-						},
-						Members: []*etcdserverpb.Member{
-							&etcdserverpb.Member{
-								ID:   1001,
-								Name: "node0",
-								PeerURLs: []string{
-									"https//10.0.0.1:8001",
-									"https//127.0.0.1:8001",
-								},
-								ClientURLs: []string{
-									"https//10.0.0.1:8081",
-									"https//127.0.0.1:8081",
-								},
-								IsLearner: false,
-							},
-							&etcdserverpb.Member{
-								ID:   1002,
-								Name: "node1",
-								PeerURLs: []string{
-									"https//10.0.0.2:8001",
-									"https//127.0.0.1:8001",
-								},
-								ClientURLs: []string{
-									"https//10.0.0.2:8081",
-									"https//127.0.0.1:8081",
-								},
-								IsLearner: false,
-							},
-							&etcdserverpb.Member{
-								ID:   1003,
-								Name: "node2",
-								PeerURLs: []string{
-									"https//10.0.0.3:8001",
-									"https//127.0.0.1:8001",
-								},
-								ClientURLs: []string{
-									"https//10.0.0.3:8081",
-									"https//127.0.0.1:8081",
-								},
-								IsLearner: false,
-							},
-						},
+				MemberRemoveResponseWithErr: &etcdutil.MemberListResponseWithErr{
+					Header: &etcdserverpb.ResponseHeader{
+						ClusterId: 10,
+						MemberId:  1,
+						Revision:  20,
+						RaftTerm:  30,
 					},
 					Err: nil,
 				},
+				MemberPromoteResponseWithErr: &etcdutil.MemberListResponseWithErr{
+					Header: &etcdserverpb.ResponseHeader{
+						ClusterId: 10,
+						MemberId:  1,
+						Revision:  20,
+						RaftTerm:  30,
+					},
+					Err: nil,
+				},
+				HealthCheckErr:    nil,
+				DefragmentErr:     nil,
+				CreateSnapshotErr: nil,
 			},
 			expectedSelf: &Member{
-				Status: &etcdserverpb.StatusResponse{
-					Header: &etcdserverpb.ResponseHeader{
-						ClusterId: 3001,
-						MemberId:  1001,
-						Revision:  5000,
-					},
-					Leader:    1001,
-					RaftIndex: 2001,
-					IsLearner: false,
-				},
-				Member: &etcdserverpb.Member{
-					ID:   1001,
-					Name: "node0",
-					PeerURLs: []string{
-						"https//10.0.0.1:8001",
-						"https//127.0.0.1:8001",
-					},
-					ClientURLs: []string{
-						"https//10.0.0.1:8081",
-						"https//127.0.0.1:8081",
-					},
-				},
-			},
-			expectedLeader: &Member{
-				Status: &etcdserverpb.StatusResponse{
-					Header: &etcdserverpb.ResponseHeader{
-						ClusterId: 3001,
-						MemberId:  1001,
-						Revision:  5000,
-					},
-					Leader:    1001,
-					RaftIndex: 2001,
-					IsLearner: false,
-				},
-				Member: &etcdserverpb.Member{
-					ID:   1001,
-					Name: "node0",
-					PeerURLs: []string{
-						"https//10.0.0.1:8001",
-						"https//127.0.0.1:8001",
-					},
-					ClientURLs: []string{
-						"https//10.0.0.1:8081",
-						"https//127.0.0.1:8081",
-					},
-				},
-			},
-			expectedMemberMap: map[uint64]*Member{
-				1001: &Member{
-					Status: &etcdserverpb.StatusResponse{
-						Header: &etcdserverpb.ResponseHeader{
-							ClusterId: 3001,
-							MemberId:  1001,
-							Revision:  5000,
-						},
-						Leader:    1001,
-						RaftIndex: 2001,
-						IsLearner: false,
-					},
-					Member: &etcdserverpb.Member{
-						ID:   1001,
-						Name: "node0",
-						PeerURLs: []string{
-							"https//10.0.0.1:8001",
-							"https//127.0.0.1:8001",
-						},
-						ClientURLs: []string{
-							"https//10.0.0.1:8081",
-							"https//127.0.0.1:8081",
-						},
-					},
-				},
-				1002: &Member{
-					Status: &etcdserverpb.StatusResponse{
-						Header: &etcdserverpb.ResponseHeader{
-							ClusterId: 3001,
-							MemberId:  1002,
-							Revision:  5001,
-						},
-						Leader:    1001,
-						RaftIndex: 2001,
-						IsLearner: false,
-					},
-					Member: &etcdserverpb.Member{
-						ID:   1002,
-						Name: "node1",
-						PeerURLs: []string{
-							"https//10.0.0.2:8001",
-							"https//127.0.0.1:8001",
-						},
-						ClientURLs: []string{
-							"https//10.0.0.2:8081",
-							"https//127.0.0.1:8081",
-						},
-					},
-				},
-				1003: &Member{
-					Status: &etcdserverpb.StatusResponse{
-						Header: &etcdserverpb.ResponseHeader{
-							ClusterId: 3001,
-							MemberId:  1003,
-							Revision:  5000,
-						},
-						Leader:    1001,
-						RaftIndex: 2001,
-						IsLearner: false,
-					},
-					Member: &etcdserverpb.Member{
-						ID:   1003,
-						Name: "node2",
-						PeerURLs: []string{
-							"https//10.0.0.3:8001",
-							"https//127.0.0.1:8001",
-						},
-						ClientURLs: []string{
-							"https//10.0.0.3:8081",
-							"https//127.0.0.1:8081",
-						},
-					},
-				},
+				Status: happyNode0.MemberResponse,
+				Member: happyNode0.StatusResponseWithErr.StatusResponse,
 			},
 		},
 	}
@@ -290,8 +201,8 @@ func TestNewStatus(t *testing.T) {
 			err := status.SyncStatus(tt.args)
 			assert.Equal(t, nil, err)
 			assert.Equal(t, tt.expectedSelf, status.Self)
-			assert.Equal(t, tt.expectedLeader, status.Leader)
-			assert.Equal(t, tt.expectedMemberMap, status.MemberMap)
+			// assert.Equal(t, tt.expectedLeader, status.Leader)
+			// assert.Equal(t, tt.expectedMemberMap, status.MemberMap)
 		})
 	}
 }
