9fc3ec10036ffa4701795072194de3ac4059be44
randomcoww <averyfuzzybunny@gmail.com>
2025-12-05 12:01:21 +0900

    update etcdprocess tests

--- pkg/etcdprocess/etcdprocess_test.go
+++ pkg/etcdprocess/etcdprocess_test.go
@@ -20,48 +20,38 @@ const (
 	etcdutlBinaryFile string = "/mnt/usr/local/bin/etcdutl"
 )
 
-func TestCreateCluster(t *testing.T) {
+func TestCreateNewCluster(t *testing.T) {
 	ctx, _ := context.WithTimeout(context.Background(), time.Duration(20*time.Second))
 
-	withCluster(t, ctx, "new", func(client etcdclient.EtcdClient) {
-		err := client.GetHealth(ctx)
+	configs := memberConfigs("new")
+	var config *c.Config
+	for _, config = range configs {
+		p := NewProcess(context.Background(), config)
+		err := p.Run()
 		assert.NoError(t, err)
-	})
+
+		defer RemoveDataDir(config)
+		defer p.Wait()
+		defer p.Stop()
+	}
+
+	client, err := etcdclient.NewClientFromPeers(ctx, config)
+	assert.NoError(t, err)
+
+	err = client.GetHealth(ctx)
+	assert.NoError(t, err)
+
+	list, err := client.MemberList(ctx)
+	assert.NoError(t, err)
+	assert.Equal(t, len(list.GetMembers()), len(configs))
 }
 
-func TestRestoreSnapshot(t *testing.T) {
+func TestCreateClusterFromSnapshotRestore(t *testing.T) {
 	ctx, _ := context.WithTimeout(context.Background(), time.Duration(20*time.Second))
-	members := []string{
-		"node0",
-		"node1",
-		"node2",
-	}
-	var initialCluster []string
-	for i, member := range members {
-		initialCluster = append(initialCluster, fmt.Sprintf("%s=https://127.0.0.1:%d", member, peerPortBase+i))
-	}
-	logger, _ := zap.NewProduction()
-	defer logger.Sync()
 
+	configs := memberConfigs("existing")
 	var config *c.Config
-	for i, member := range members {
-		testDir := filepath.Join("test", member+".etcd")
-
-		config = &c.Config{
-			EtcdutlBinaryFile: etcdutlBinaryFile,
-			Logger:            logger,
-			Env: map[string]string{
-				"ETCD_DATA_DIR":                    testDir,
-				"ETCD_NAME":                        member,
-				"ETCD_TRUSTED_CA_FILE":             filepath.Join("test", "ca-cert.pem"),
-				"ETCD_CERT_FILE":                   filepath.Join("test", member, "client", "cert.pem"),
-				"ETCD_KEY_FILE":                    filepath.Join("test", member, "client", "key.pem"),
-				"ETCD_INITIAL_ADVERTISE_PEER_URLS": fmt.Sprintf("https://127.0.0.1:%d", peerPortBase+i),
-				"ETCD_INITIAL_CLUSTER":             strings.Join(initialCluster, ","),
-				"ETCD_INITIAL_CLUSTER_TOKEN":       "test",
-			},
-		}
-
+	for _, config = range configs {
 		err := RestoreV3Snapshot(ctx, config, filepath.Join("test", "snapshot.db"))
 		assert.NoError(t, err)
 
@@ -69,11 +59,27 @@ func TestRestoreSnapshot(t *testing.T) {
 		assert.NoError(t, err)
 		assert.True(t, ok)
 
+		p := NewProcess(context.Background(), config)
+		err = p.Run()
+		assert.NoError(t, err)
+
 		defer RemoveDataDir(config)
+		defer p.Wait()
+		defer p.Stop()
 	}
+
+	client, err := etcdclient.NewClientFromPeers(ctx, config)
+	assert.NoError(t, err)
+
+	err = client.GetHealth(ctx)
+	assert.NoError(t, err)
+
+	list, err := client.MemberList(ctx)
+	assert.NoError(t, err)
+	assert.Equal(t, len(list.GetMembers()), len(configs))
 }
 
-func withCluster(t *testing.T, ctx context.Context, state string, handler func(etcdclient.EtcdClient)) {
+func memberConfigs(state string) []*c.Config {
 	members := []string{
 		"node0",
 		"node1",
@@ -84,15 +90,15 @@ func withCluster(t *testing.T, ctx context.Context, state string, handler func(e
 		initialCluster = append(initialCluster, fmt.Sprintf("%s=https://127.0.0.1:%d", member, peerPortBase+i))
 	}
 	logger, _ := zap.NewProduction()
-	defer logger.Sync()
 
-	var config *c.Config
+	var configs []*c.Config
 	for i, member := range members {
 		testDir := filepath.Join("test", member+".etcd")
 
-		config = &c.Config{
-			EtcdBinaryFile: "/mnt/usr/local/bin/etcd",
-			Logger:         logger,
+		config := &c.Config{
+			EtcdBinaryFile:    etcdBinaryFile,
+			EtcdutlBinaryFile: etcdutlBinaryFile,
+			Logger:            logger,
 			Env: map[string]string{
 				"ETCD_DATA_DIR":                    testDir,
 				"ETCD_NAME":                        member,
@@ -115,17 +121,7 @@ func withCluster(t *testing.T, ctx context.Context, state string, handler func(e
 			},
 		}
 		config.ParseEnvs()
-
-		p := NewProcess(context.Background(), config)
-		err := p.Run()
-		assert.NoError(t, err)
-
-		defer RemoveDataDir(config)
-		defer p.Wait()
-		defer p.Stop()
+		configs = append(configs, config)
 	}
-
-	client, err := etcdclient.NewClientFromPeers(ctx, config)
-	assert.NoError(t, err)
-	handler(client)
+	return configs
 }
