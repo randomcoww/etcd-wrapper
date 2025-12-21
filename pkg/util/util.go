89e084e172b9075514d5d0fd51b9c5010b5af7f1
randomcoww <averyfuzzybunny@gmail.com>
2025-12-21 09:33:54 -0800

    remove unused

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,35 +1,5 @@
 package util
 
-import (
-	"io"
-	"os"
-	"path/filepath"
-)
-
-func WriteFile(rc io.Reader, path string) error {
-	err := os.MkdirAll(filepath.Dir(path), os.FileMode(0644))
-	if err != nil {
-		return err
-	}
-
-	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	_, err = io.Copy(f, rc)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func DeleteFile(path string) error {
-	return os.Remove(path)
-}
-
 func HasMatchingElement[T comparable](s1, s2 []T) bool {
 	elementsInS1 := make(map[T]struct{})
 	for _, v := range s1 {
