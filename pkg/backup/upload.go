1dfc50850b7282f2dfddbc8fd279a085728f3d96
randomcoww <averyfuzzybunny@gmail.com>
2026-08-13 22:59:34 -0700

    handle and return uploaded file size

--- pkg/backup/upload.go
+++ pkg/backup/upload.go
@@ -9,15 +9,16 @@ import (
 	"io"
 )
 
-func UploadSnapshot(ctx context.Context, config *c.Config, s3 s3client.Client, reader io.Reader, tagFunc func() string) error {
-	if err := s3.Upload(ctx, config, fmt.Sprintf("%s%s", config.S3BackupKeyPrefix, tagFunc()), reader); err != nil {
+func UploadSnapshot(ctx context.Context, config *c.Config, s3 s3client.Client, reader io.Reader, tagFunc func() string) (int64, error) {
+	size, err := s3.Upload(ctx, config, fmt.Sprintf("%s%s", config.S3BackupKeyPrefix, tagFunc()), reader)
+	if err != nil {
 		config.Logger.Error("upload backup failed", zap.Error(err))
-		return err
+		return size, err
 	}
 
 	keys := s3.List(ctx, config)
 	if len(keys) > config.S3BackupCount {
-		return s3.Remove(ctx, config, keys[:len(keys)-config.S3BackupCount])
+		return size, s3.Remove(ctx, config, keys[:len(keys)-config.S3BackupCount])
 	}
-	return nil
+	return size, nil
 }
